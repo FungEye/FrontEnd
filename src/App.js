96d8525216fc899eb6802f466e0ade3752f1adb5
KamilFischbach <fischbach.kamil@gmail.com>
2023-04-29 17:01:49 +0200

    TG-75 Authentication working /routes secured

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import { Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Box from "./components/Box";
 import Dashboard from "./components/Dashboard";
+import RegisterLogin from "./components/RegisterLogin";
+import { useIsAuthenticated } from "react-auth-kit";
 
 function App() {
+  const isAuthenticated = useIsAuthenticated();
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/box" element={<Box />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/box"
+          element={isAuthenticated() ? <Box /> : <RegisterLogin />}
+        ></Route>
+        <Route
+          path="/dashboard"
+          element={isAuthenticated() ? <Dashboard /> : <RegisterLogin />}
+        />
+        <Route path="/login" element={<RegisterLogin />} />
       </Routes>
     </div>
   );
 }
-
-
 
 export default App;
