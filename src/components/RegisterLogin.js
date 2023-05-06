fb38b83832ae547d3a3f0081d0d7980226406273
KamilFischbach <fischbach.kamil@gmail.com>
2023-05-06 16:56:19 +0200

    TG-75 FIX using let instead of var

--- src/components/RegisterLogin.js
+++ src/components/RegisterLogin.js
@@ -56,7 +56,7 @@ export default function RegisterLogin() {
 
   //source: https://www.educba.com/javascript-hash/
   function toHash(string) {
-    var hash = 0;
+    let hash = 0;
     if (string.length === 0) return hash;
     for (let i = 0; i < string.length; i++) {
       let ch = string.charCodeAt(i);
