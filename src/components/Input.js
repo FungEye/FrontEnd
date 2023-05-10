be8fa0bbc2427178583063155d0370b5ca3c0708
Laura Rebelo <laura.b.rebelo@gmail.com>
2023-05-10 13:23:07 +0200

    FIXED input styling once more, needed flexbox column property

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,17 +2,23 @@ import "./css/Input.css";
 import "./css/General.css";
 
 function Input(props) {
+
+  let classes = "input rounded-20 poppins text-dark";
+  if (props.wide) {
+    classes += " wide";
+  }
+
   return (
-    <>
+    <div className="column">
       <label className="poppins text-dark"><b>{props.title}</b></label>
       <input
-        className="input rounded-20 poppins text-dark"
+        className= {classes}
         value={props.value}
         onChange={props.onChange}
         type={props.type}
         onKeyPress={props.onKeyPress}
       />
-    </>
+    </div>
   );
 }
 
