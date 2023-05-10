bd8159157f01aa7dd03226457c81bd3aeb4cb7e9
Laura Rebelo <laura.b.rebelo@gmail.com>
2023-05-10 13:09:34 +0200

    Restyled input field

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,9 +4,9 @@ import "./css/General.css";
 function Input(props) {
   return (
     <>
-      <span className="poppins text-dark">{props.title}</span>
+      <label className="poppins text-dark"><b>{props.title}</b></label>
       <input
-        className="input rounded-20 poppins"
+        className="input rounded-20 poppins text-dark"
         value={props.value}
         onChange={props.onChange}
         type={props.type}
