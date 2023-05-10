006a9ec9bce482d97052d3a54e1e9c3ee3a7e715
Laura Rebelo <laura.b.rebelo@gmail.com>
2023-05-10 13:24:30 +0200

    FIXED top part of Add New Species

--- src/components/AddNewSpecies.js
+++ src/components/AddNewSpecies.js
@@ -3,6 +3,7 @@ import "./css/AddNewSpecies.css";
 import { useState } from 'react';
 import ButtonSecondary from "./ButtonSecondary";
 import ButtonPrimary from "./ButtonPrimary";
+import Input from "./Input";
 
 function AddNewSpecies(){
   const [name, setName] = useState('');
@@ -13,17 +14,21 @@ function AddNewSpecies(){
         <div className="cont column varela bg-light rounded-20 column jc-center very-slightly-faded border-dark">
           <form onSubmit={handleFormSubmit}>
       <div className="dashboard column align-items-center">
-      <div className="mushroom-title text-dark ultra">Add new species:</div>
-      <div class="info text-dark"> <p> Provide ideal growing conditions for each phase...</p></div>
+      <div id="new-species-title" className="mushroom-title text-dark ultra">Add new species:</div>
+      <Input
+                  title="Mushroom name"
+                  placeholder="Mushroom name..."
+                  value={name}
+                  type="mushroom-name"
+                  onChange={(event) => {
+                    setName(event.target.value);
+                    // setError("");
+                  }}
+                  wide={true}
+                />
+      <div class="ans-info text-dark">Provide ideal growing conditions for each phase...</div>
       <div className="name text-dark row jc-space-evenly align-items-center w-100 "> 
-      <label>
-             Mushroom name:
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </label>
+      
         </div>
         <div className="phases">
             <AddNewSpeciesForm title="Phase 1:"/>
@@ -33,7 +38,7 @@ function AddNewSpecies(){
         </div>
         <div className="text-dark row jc-space-evenly align-items-center w-100 "> 
         <ButtonSecondary text="Back" />
-        <ButtonPrimary text="Add Mushroom" />
+        <ButtonPrimary wide={true} text="Add Mushroom" />
         </div>
       </div>
       </form>
