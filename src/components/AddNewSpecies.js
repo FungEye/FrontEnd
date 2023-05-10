12cdaecc04e9434529843e2c7cf5bd546df110ac
Ignas Druskinis <ignas.druskinis@gmail.com>
2023-05-10 02:00:50 +0200

    added new css, logic

--- src/components/AddNewSpecies.js
+++ src/components/AddNewSpecies.js
@@ -3,18 +3,35 @@ import "./css/AddNewSpecies.css";
 import { useState } from 'react';
 
 function AddNewSpecies(){
-
+  const [name, setName] = useState('');
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+  }
     return(
         <div className="cont column varela bg-light rounded-20 column jc-center very-slightly-faded border-dark">
+          <form onSubmit={handleFormSubmit}>
       <div className="dashboard column align-items-center">
       <div className="mushroom-title text-dark ultra">Add new mushrooms</div>
-      <div className="name"> placeholder for name</div>
-      <div className="ultra text-dark">Ideal growth conditions</div>
+      <div className="name">
+        <label>
+             Mushroom name:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            </div>
       
-        <div className="carousel ">
-            <AddNewSpeciesForm/>
+        <div className="phases">
+            <AddNewSpeciesForm title="Phase 1"/>
+            <AddNewSpeciesForm title="Phase 2"/>
+            <AddNewSpeciesForm title="Phase 3"/>
+            <AddNewSpeciesForm title="Phase 4"/>
         </div>
+        <button type="submit" className="button bg-dark text-light poppins raise faded">Add Mushroom</button>
       </div>
+      </form>
     </div>
     );
        
--- src/components/AddNewSpeciesForm.js
+++ src/components/AddNewSpeciesForm.js
@@ -1,175 +1,75 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from "react";
+function AddNewSpeciesForm(props){
 
-
-const AddMushroomForm = () => {
-  const [name, setName] = useState('');
+  let title = props.title;
   const [temperature, setTemperature] = useState('');
   const [humidity, setHumidity] = useState('');
   const [co2, setCo2] = useState('');
   const [lux, setLux] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  
+  const handleTemperatureChange = (event) => {
+    setTemperature(event.target.value);
   };
 
+  const handleHumidityChange = (event) => {
+    setHumidity(event.target.value);
+  };
+
+  const handleCo2Change = (event) => {
+    setCo2(event.target.value);
+  };
+
+  const handleLightChange = (event) => {
+    setLux(event.target.value);
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setTemperature('');
+    setHumidity('');
+    setCo2('');
+    setLux('');
+  }
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="carousel">
-        <div className="carousel-inner">
-      
-
-          <div className="carousel-item">
-            <h3>Phase 1</h3>
-            <label>
+      <div className="bomb">
+          <h2 className="ultra text-dark">{title}</h2>
+          <label>
               Temperature:
               <input
-                type="text"
+                type="number"
+                id="temperature"
                 value={temperature}
-                onChange={(e) => setTemperature(e.target.value)}
+                onChange={handleTemperatureChange}
               />
             </label>
             <label>
               Humidity:
               <input
-                type="text"
+                type="number"
+                id="humidity"
                 value={humidity}
-                onChange={(e) => setHumidity(e.target.value)}
+                onChange={handleHumidityChange}
               />
             </label>
             <label>
               CO2:
               <input
-                type="text"
+                type="number"
+                id="co2"
                 value={co2}
-                onChange={(e) => setCo2(e.target.value)}
+                onChange={handleCo2Change}
               />
             </label>
             <label>
               Light:
               <input
-                type="text"
+                type="number"
+                id="lux"
                 value={lux}
-                onChange={(e) => setLux(e.target.value)}
+                onChange={handleLightChange}
               />
             </label>
-          </div>
+      </div>)
 
-         
-          <div className="carousel-item">
-            <h3>Phase 2</h3>
-            <label>
-              Temperature:
-              <input
-                type="text"
-                value={temperature}
-                onChange={(e) => setTemperature(e.target.value)}
-              />
-            </label>
-            <label>
-              Humidity:
-              <input
-                type="text"
-                value={humidity}
-                onChange={(e) => setHumidity(e.target.value)}
-              />
-            </label>
-            <label>
-              CO2:
-              <input
-                type="text"
-                value={co2}
-                onChange={(e) => setCo2(e.target.value)}
-              />
-            </label>
-            <label>
-              Light:
-              <input
-                type="text"
-                value={lux}
-                onChange={(e) => setLux(e.target.value)}
-              />
-            </label>
-          </div>
+}
 
-    
-          <div className="carousel-item">
-            <h3>Phase 3</h3>
-            <label>
-              Temperature:
-              <input
-                type="text"
-                value={temperature}
-                onChange={(e) => setTemperature(e.target.value)}
-              />
-            </label>
-            <label>
-              Humidity:
-              <input
-                type="text"
-                value={humidity}
-                onChange={(e) => setHumidity(e.target.value)}
-              />
-            </label>
-            <label>
-              CO2:
-              <input
-                type="text"
-                value={co2}
-                onChange={(e) => setCo2(e.target.value)}
-              />
-            </label>
-            <label>
-              Light:
-              <input
-                type="text"
-                value={lux}
-                onChange={(e) => setLux(e.target.value)}
-              />
-            </label>
-          </div>
-
-        
-          <div className="carousel-item">
-            <h3>Phase 4</h3>
-            <label>
-              Temperature:
-              <input
-                type="text"
-                value={temperature}
-                onChange={(e) => setTemperature(e.target.value)}
-              />
-            </label>
-            <label>
-              Humidity:
-              <input
-                type="text"
-                value={humidity}
-                onChange={(e) => setHumidity(e.target.value)}
-              />
-            </label>
-            <label>
-              CO2:
-              <input
-                type="text"
-                value={co2}
-                onChange={(e) => setCo2(e.target.value)}
-              />
-            </label>
-            <label>
-              Light:
-              <input
-                type="text"
-                value={lux}
-                onChange={(e) => setLux(e.target.value)}
-              />
-            </label>
-          </div>
-        </div>
-      </div>
-
-      <button type="submit">Add Mushroom</button>
-    </form>
-  );
-};
-
-export default AddMushroomForm;
+export default AddNewSpeciesForm;
