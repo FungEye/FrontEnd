16fb3ea2384aed589b6bcafade6bf4fc1bd24ef5
KamilFischbach <fischbach.kamil@gmail.com>
2023-05-15 18:23:59 +0200

    ADD trying out cypress 2

--- cypress/e2e/overview.cy.js
+++ cypress/e2e/overview.cy.js
@@ -5,7 +5,7 @@ describe('Overview PAGE test suite', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost:3000/#/login')
+    cy.visit('http://localhost:3000/Frontend/#/login')
     cy.get("[data-test='Username']").type("lol");
     cy.get("[data-test='Password']").type("lalalala");
     cy.get("[data-test='Login']").click();
