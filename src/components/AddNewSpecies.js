29e505fd8a6c50494524fd5f1a9faac4c00a6006
KamilFischbach <fischbach.kamil@gmail.com>
2023-05-17 09:11:17 +0200

    TG-165 FIX imports

--- src/components/AddNewSpecies.js
+++ src/components/AddNewSpecies.js
@@ -1,55 +1,52 @@
 import AddNewSpeciesForm2 from "./AddNewSpeciesForm2";
 import "./css/AddNewSpecies.css";
-import { useState } from 'react';
-import ButtonSecondary from "./ButtonSecondary";
 import ButtonPrimary from "./ButtonPrimary";
-import Input from "./Input";
 import XButton from "./XButton";
 import AddNewSpeciesOrigin from "./AddNewSpeciesOrigin";
 
 function AddNewSpecies() {
-  const [name, setName] = useState('');
-  const [origin, setOrigin] = useState('');
-  const [imageurl, setImageurl] = useState('');
-  const [spawningConditions, setSpawningConditions] = useState(getNewConditions());
-  const [piningConditions, setPiningConditions] = useState(getNewConditions());
-  const [fruitingConditions, setFruitingConditions] = useState(getNewConditions());
+  // const [name, setName] = useState('');
+  // const [origin, setOrigin] = useState('');
+  // const [imageurl, setImageurl] = useState('');
+  // const [spawningConditions, setSpawningConditions] = useState(getNewConditions());
+  // const [piningConditions, setPiningConditions] = useState(getNewConditions());
+  // const [fruitingConditions, setFruitingConditions] = useState(getNewConditions());
 
-  function getNewConditions() {
-    return {
-      "temperature": 0,
-      "humidity": 0,
-      "co2": 0,
-      "light": 0
-    }
-  }
-  function clearName(textarea) {
-    if (textarea.value === textarea.defaultValue) {
-      textarea.value = '';
-    }
-  }
+  // function getNewConditions() {
+  //   return {
+  //     "temperature": 0,
+  //     "humidity": 0,
+  //     "co2": 0,
+  //     "light": 0
+  //   }
+  // }
+  // function clearName(textarea) {
+  //   if (textarea.value === textarea.defaultValue) {
+  //     textarea.value = '';
+  //   }
+  // }
 
-  function updateCondition(oldCondition, stat, value) {
-    let newCondition = {...oldCondition};
-    switch (stat) {
-      case "temp":
-        newCondition.temperature = value;
-        break;
-      case "humidity":
-        newCondition.humidity = value;
-        break;
-      case "co2":
-        newCondition.co2 = value;
-        break;
-      case "light":
-        newCondition.light = value;
-        break;
-      default:
-        return;
-        
-    }
-    return newCondition;
-  }
+  // function updateCondition(oldCondition, stat, value) {
+  //   let newCondition = {...oldCondition};
+  //   switch (stat) {
+  //     case "temp":
+  //       newCondition.temperature = value;
+  //       break;
+  //     case "humidity":
+  //       newCondition.humidity = value;
+  //       break;
+  //     case "co2":
+  //       newCondition.co2 = value;
+  //       break;
+  //     case "light":
+  //       newCondition.light = value;
+  //       break;
+  //     default:
+  //       return;
+
+  //   }
+  //   return newCondition;
+  // }
 
   return (
     <div className="cont column varela bg-light rounded-20 column jc-center very-slightly-faded border-dark">
@@ -57,19 +54,21 @@ function AddNewSpecies() {
         <XButton />
       </div>
       <div className="dashboard column align-items-center">
-        <div id="new-species-title" className="mushroom-title text-dark ultra">Add new species</div>
+        <div id="new-species-title" className="mushroom-title text-dark ultra">
+          Add new species
+        </div>
         <div className="origin-row row">
-          <AddNewSpeciesOrigin/>
+          <AddNewSpeciesOrigin />
           <div className="origindescription" placeholder="Description">
-            <textarea rows="10" cols="30" placeholder="Description">
-  
-            </textarea>
+            <textarea rows="10" cols="30" placeholder="Description"></textarea>
           </div>
         </div>
-        <div className="ans-info text-dark">Provide ideal growing conditions for each phase...</div>
+        <div className="ans-info text-dark">
+          Provide ideal growing conditions for each phase...
+        </div>
         <div className="phases column align-items-center gap-20">
           <div className="row gap-20">
-            <AddNewSpeciesForm2 title="Spawning"/>
+            <AddNewSpeciesForm2 title="Spawning" />
             <AddNewSpeciesForm2 title="Pining" />
           </div>
           <AddNewSpeciesForm2 title="Fruiting" />
@@ -80,7 +79,6 @@ function AddNewSpecies() {
       </div>
     </div>
   );
-
 }
 
 export default AddNewSpecies;
--- src/components/ArchiveModal.js
+++ src/components/ArchiveModal.js
@@ -3,40 +3,39 @@ import ButtonPrimary from "./ButtonPrimary";
 import ButtonSecondary from "./ButtonSecondary";
 import XButton from "./XButton";
 
+function ArchiveModal(props) {
+  if (!props.show) {
+    return null;
+  }
 
-
-function ArchiveModal(props){
-    if (!props.show){
-        return null
-    }
-
-    return(
-        <div className="md-modal" onClick={props.onClose}>
-            <div className="md-modal-content" onClick={e => e.stopPropagation()}>
-                <div className="md-modal-header align-items-center bg-dark row jc-space-between text-light poppins very-slightly-faded">
-                    <div>Archive confirmation:</div><XButton onClick={props.onClose}/>
-                </div>
-                <div className="md-modal-body bg-light column very-slightly-faded">
-                    <div className="md-modal-body-top row align-items-center jc-space-between">
-                    </div>
-                    <div className="md-modal-body-info row jc-space-between">
-                        <div id="md-modal-body-left"className="column jc-space-between">
-                            <div className="md-modal-info-text column text-dark">
-                                <div>
-                                    <b class="text-dark poppins">Are you sure you want to archive this mushroom? </b>
-                                    </div>
-                             </div>
-                             <div className="row btns jc-center">
-                                <ButtonSecondary text="Archive" />
-                                <ButtonSecondary text="Back" onClick={props.onClose}/>
-                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="md-modal" onClick={props.onClose}>
+      <div className="md-modal-content" onClick={(e) => e.stopPropagation()}>
+        <div className="md-modal-header align-items-center bg-dark row jc-space-between text-light poppins very-slightly-faded">
+          <div>Archive confirmation:</div>
+          <XButton onClick={props.onClose} />
         </div>
-    )
-
+        <div className="md-modal-body bg-light column very-slightly-faded">
+          <div className="md-modal-body-top row align-items-center jc-space-between"></div>
+          <div className="md-modal-body-info row jc-space-between">
+            <div id="md-modal-body-left" className="column jc-space-between">
+              <div className="md-modal-info-text column text-dark">
+                <div>
+                  <b class="text-dark poppins">
+                    Are you sure you want to archive this mushroom?{" "}
+                  </b>
+                </div>
+              </div>
+              <div className="row btns jc-center">
+                <ButtonSecondary text="Back" onClick={props.onClose} />
+                <ButtonPrimary text="Archive" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ArchiveModal;
