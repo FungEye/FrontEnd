6b7e0ca21112c88ea2afdb03fbb01e66056f055b
KamilFischbach <fischbach.kamil@gmail.com>
2023-05-22 16:13:41 +0200

    tg-133 useful links navigate

--- src/components/history/HistoryUseful.js
+++ src/components/history/HistoryUseful.js
@@ -1,6 +1,8 @@
 import "../css/History.css";
 import "../css/General.css";
+import { useNavigate } from "react-router-dom";
 export default function HistoryUseful() {
+  const navigate = useNavigate();
   return (
     <div className="usefulContainer p-10 column bg-med rounded-10 border-dark">
       <div className="row jc-space-between align-items-center">
@@ -9,10 +11,18 @@ export default function HistoryUseful() {
         <p>🍄</p>
       </div>
       <div className="pagesContainer">
-        <div className=" usefulLinks textHover varela">
+        <div
+          className=" usefulLinks textHover varela"
+          onClick={() => navigate("/mushrooms")}
+        >
           >See mushroom details
         </div>
-        <div className=" usefulLinks textHover varela">>See recipes</div>
+        <div
+          className=" usefulLinks textHover varela"
+          onClick={() => navigate("/recipes")}
+        >
+          >See recipes
+        </div>
       </div>
     </div>
   );
