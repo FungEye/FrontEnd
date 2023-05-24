0ef7ff8139aa06a59971a95528f38dda79181dcb
Robert Barta <robert.barta314@gmail.com>
2023-05-24 23:37:26 +0200

    FIXED Centered the welcome page, and removed the footer since it is not used anywhere else.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,25 +11,21 @@ function Welcome() {
   }
 
   return (
-    <div>
-      <div className="welcome-container">
-        <h1 className="welcome-text">
-          <Typewriter
-            options={{
-              autoStart: true,
-              loop: true,
-              delay: 100,
-              strings: ["Welcome to FungEye!"],
-            }}
-          />
-        </h1>
-        <h3 className="motto">Empowering you to grow mushrooms like a pro. </h3>
-        <button className="button-54" onClick={handleClick}>
-          Go to dashboard!
-        </button>
-      </div>
-
-      <div className="footer">Fungeye 2023</div>
+    <div className="welcome-container">
+      <h1 className="welcome-text">
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+            delay: 100,
+            strings: ["Welcome to FungEye!"],
+          }}
+        />
+      </h1>
+      <h3 className="motto">Empowering you to grow mushrooms like a pro. </h3>
+      <button className="button-54" onClick={handleClick}>
+        Go to dashboard!
+      </button>
     </div>
   );
 }
