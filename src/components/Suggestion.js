e101b4b8056481146eaf815a326c086eb1c8e415
Laura Rebelo <laura.b.rebelo@gmail.com>
2023-05-26 10:24:33 +0200

    removed a useless import and a useless js file

--- src/components/Suggestion.js
+++
@@ -1,9 +0,0 @@
-// function Suggestion(props) {
-//     return (
-//         <div className="footer border-dark">
-//           <h3 className="ultra text-dark">Ideal Temperature:</h3>
-//           <p className="ultra text-dark">? &deg;C</p>
-//           <p className="ultra text-dark">Suggestion:</p>
-//         </div>
-//     )
-// }
